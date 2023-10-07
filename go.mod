c2f6b891824c2bbaf13509c96880e8500cf37db8
Tiago rosa da costa <tiagorosadacost@gmail.com>
2023-10-07 11:54:13 -0300

    Send code to remote repository

---
+++ go.mod
@@ -0,0 +1,9 @@
+module poc-publish-sqs-golang
+
+go 1.18
+
+require (
+	github.com/aws/aws-sdk-go v1.45.24 // indirect
+	github.com/google/uuid v1.3.1 // indirect
+	github.com/jmespath/go-jmespath v0.4.0 // indirect
+)
---
+++ main.go
@@ -0,0 +1,72 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"sync"
+	"time"
+
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/sqs"
+	"github.com/google/uuid"
+)
+
+type EmailMessage struct {
+	To      string `json:"to"`
+	Content string `json:"content"`
+}
+
+func processInBatch(sqsQueue *sqs.SQS, guard <-chan struct{}, wt *sync.WaitGroup, total int) {
+
+	messages := []*sqs.SendMessageBatchRequestEntry{}
+	for index := 0; index < total; index++ {
+		emailToJson, _ := json.Marshal(EmailMessage{
+			To:      "tiago@gmail.com",
+			Content: "Hi my friend",
+		})
+		emailToString := string(emailToJson)
+		id := uuid.NewString()
+
+		messages = append(messages, &sqs.SendMessageBatchRequestEntry{
+			Id:          &id,
+			MessageBody: &emailToString,
+		})
+
+		if len(messages) == 10 {
+			sqsQueue.SendMessageBatch(&sqs.SendMessageBatchInput{
+				Entries:  messages,
+				QueueUrl: aws.String("https://sqs.us-east-1.amazonaws.com/507403822990/scale-producer"),
+			})
+
+			messages = []*sqs.SendMessageBatchRequestEntry{}
+		}
+
+	}
+	wt.Done()
+	<-guard
+}
+
+func main() {
+	start := time.Now()
+	sess, _ := session.NewSessionWithOptions(session.Options{
+		Profile: "tiago",
+		Config: aws.Config{
+			Region: aws.String("us-east-1"),
+		},
+	})
+
+	sqsQueue := sqs.New(sess)
+
+	maxGoroutines := 80
+	guard := make(chan struct{}, maxGoroutines)
+	var wt sync.WaitGroup
+	for index := 0; index < 100000; index += 500 {
+		wt.Add(1)
+		guard <- struct{}{}
+		go processInBatch(sqsQueue, guard, &wt, 500)
+	}
+
+	wt.Wait()
+	fmt.Println(time.Since(start))
+}
